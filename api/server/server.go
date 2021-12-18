72a42f3634190809e4032882ae45544ec3e76a8a
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-18 23:40:21 +0900

    image_uploader.protoの定義

--- api/server/server.go
+++ api/server/server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	"github.com/xxarupakaxx/grpc-api/api/gen/api"
 	"github.com/xxarupakaxx/grpc-api/api/handler"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
@@ -18,9 +24,24 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen :%v",err)
 	}
+	zapLogger ,err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
 
-	server := grpc.NewServer()
+	grpc_zap.ReplaceGrpcLogger(zapLogger)
+
+
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				grpc_zap.UnaryServerInterceptor(zapLogger),
+				grpc_auth.UnaryServerInterceptor(auth),
+				),
+			),
+		)
   	api.RegisterPancakeBakerServiceServer(server,handler.NewBakerHandler())
+
 	reflection.Register(server)
 
 	go func() {
@@ -38,4 +59,18 @@ func main() {
 	<- quit
 	log.Println("stopping gRPC server...")
 	server.GracefulStop()
+}
+
+func auth(ctx context.Context) (context.Context, error) {
+	token ,err := grpc_auth.AuthFromMD(ctx,"bearer")
+	if err != nil {
+		return nil, err
+	}
+
+	if token != "hi/mi/tsu" {
+		return nil,grpc.Errorf(codes.Unauthenticated,"invalid bearer token")
+
+	}
+
+	return context.WithValue(ctx,"UserName","God"),nil
 }
